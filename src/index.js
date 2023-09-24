06b8e0171bda17069f386656405143d1c176afa2
Aditya Gupta <72199742+Adityagupta1625@users.noreply.github.com>
2023-09-24 09:10:16 +0530

    folder restructure

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv=require('dotenv')
+dotenv.config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const r = require("./routes/index.js");
-app.listen(8000, () => {
-  console.log("server started");
+const confrenceModule = require("./confrenceModule/routes/index");
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
 });
 
-app.use("/", r);
+app.use("/confrenceModule", confrenceModule);
 
-const MONGO_URI =
-  "mongodb+srv://Arshdeep:A1r2s3d4e5@cluster0.441ajgx.mongodb.net/ConfrenceModules";
 
-const db = mongoose
-  .connect(MONGO_URI)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(8000, () => {
+    console.log("server started");
+
+    mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("connection open!!");
   })
   .catch((err) => {
     console.log(err);
   });
+
+});
